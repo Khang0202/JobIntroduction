4e8e7a1bef54ef215f5a94b0d6a0b55c995e61c9
Khang0202 <nguyenmanhkhang2002@gmail.com>
2023-09-08 23:00:17 +0700

    a

--- JobIntroduction/target/generated-sources/annotations/com/oujava/pojo/Job_.java
+++ JobIntroduction/target/generated-sources/annotations/com/oujava/pojo/Job_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-08T22:32:14")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-08T22:59:02")
 @StaticMetamodel(Job.class)
 public class Job_ { 
 
--- JobIntroduction/target/generated-sources/annotations/com/oujava/pojo/Permission_.java
+++ JobIntroduction/target/generated-sources/annotations/com/oujava/pojo/Permission_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-08T22:32:14")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-08T22:59:02")
 @StaticMetamodel(Permission.class)
 public class Permission_ { 
 
